a90c27421a93cf0f8131269080a167de4b9926f2
guramijibladze <guramijibladze@gmail.com>
2020-08-26 17:15:34 +0400

    up

---
+++ resort/src/components/dist/StyledHero.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _styledComponents = _interopRequireDefault(require("styled-components"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n    color: red;\n    background: green;\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+var SimpleButton = _styledComponents["default"].button(_templateObject());
+
+var _default = SimpleButton;
+exports["default"] = _default;
